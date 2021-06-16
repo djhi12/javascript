760f62c917c80163f1cb55b1f141a89a8a92d5f5
djhi12 <ido20002@byui.edu>
2021-06-16 22:02:29 +0800

    initial commit

--- chapter_3_array_logic_loops/chapter_3.js
+++ chapter_3_array_logic_loops/chapter_3.js
@@ -239,9 +239,348 @@ Weak Sets
 reference to the object is removed. The technical term for this is the object is prevented from being 
 garbage-collected, which can cause a memory leak.
 */
-pokemonCharacters = null; //Error:Assignment to constant variable.
+//pokemonCharacters = null; //Error:Assignment to constant variable.
 const pokemonCharacters_1 = new Set().add(pokemonCharacters);
 console.log(pokemonCharacters_1);
+
+
+/*
+Memory Leaks
+1. A memory leak occurs when a program retains references to values that can no longer be accessed in its memory. 
+This means that memory is being used to store values that are no longer required by the program, effectively 
+wasting system resources.
+2. Memory leaks can cause problems by gradually reducing the overall memory available, which can cause the program, 
+or even the entire system, to run more slowly.
+3. 
+*/
+const animals = ['Cats', 'Dogs', 'Snake', 'Tiger'];
+const weak = new WeakSet();
+console.log(animals);
+weak.add(animals);
+console.log(weak);
+weak.has(animals);
+console.log(animals);
+weak.delete(animals);
+console.log(animals);
+weak.has(animals);
+console.log(animals);
+
+
+/*
+Creating Maps
+1. Objects are limited to using strings for key values, whereas maps can use any data type as a key.
+
+2. There is no efficient way to find the number of key-value pairs an object has, whereas this is easy to do 
+with maps using the size property.
+
+3. Objects have methods that can be called (see Chapter 5) and prototypes that can be used to create a chain 
+of inheritance (see Chapter 12), whereas maps are solely focused on the storage and retrieval of key-value pairs.
+
+4. The value of an object's properties can be accessed directly, whereas maps restrict you to using the get() 
+method to retrieve any values.
+*/
+
+const romanNumerals = new Map();
+romanNumerals.set(1, 'I').set(2, 'II').set(3, 'III').set(4, 'IV').set(5, 'V');
+console.log(romanNumerals);
+
+
+/*
+Map Methods
+1. A map is a bit like a dictionary where you can look up a value based on the key. To look up a value, we can 
+use the get() method:
+*/
+
+console.log(romanNumerals.has(1)); //true
+console.log(romanNumerals.has(7)); //false
+
+
+/*
+1. A map can be created with multiple values by using a nested array as a parameter:
+
+2. The number of key and value pairs in a map can be found by querying the size property:
+*/
+const animeCharacters = new Map([['Himawari', 'Sarada', 'Boruto', 'Sasuke', 'Naruto'], 
+['Sena', 'Yoichi', 'Suzuna', 'Ryokan']]);
+console.log(animeCharacters);
+console.log(animeCharacters.size);
+
+
+/*
+Removing Entries From Maps
+1. The delete() method can be used to remove a key and value pair from a map. This returns a boolean value of 
+true if the value was removed or false if it wasn't in the map.
+2. To delete a specific value, you need to specify the key in parentheses:
+*/
+console.log(animeCharacters.delete('Orochimaru')); //false
+console.log(animeCharacters.delete('Himawari')); //true
+
+
+/*
+clear() method
+1. The clear() method will remove all key and value pairs from a map:
+*/
+console.log(animeCharacters.clear()); //undefined
+console.log(animeCharacters.size); //0
+
+
+/*
+Converting Maps to Arrays
+1. Maps can be converted into a nested array of key-value pairs in a similar way to sets; using either the 
+spread operator:
+*/
+console.log(...romanNumerals);
+console.log(Array.from(romanNumerals));
+
+
+/*
+Weak Maps
+1. eak maps work in the same way as weak sets. They are the same as maps, except their keys cannot be primitives, 
+and the garbage collector will automatically remove any dead entries when the reference to the original object is 
+deleted.
+*/
+const Weak = new WeakMap();
+console.log(Weak);
+
+
+/*
+if Statements
+1. The code inside the block will only run if the condition in parentheses is true.
+2. 
+*/
+let notice;
+const age = 12;
+if (age < 18) {
+notice = 'Sorry, you are not old enough to play this game';
+console.log(notice);
+}
+
+
+/*
+ else Statements
+1. The else keyword can be used to add an alternative block of code to run if the condition is false.
+*/
+let noticed;
+const aged = 20;
+if (aged < 18) {
+   noticed = 'Sorry, you are not old enough to play this game';
+} else {
+   noticed = 'You\'re old enough to play this game';
+}
+console.log(noticed);
+
+
+
+const n = 12;
+if (n%2 === 0) {
+console.log('n is an even number');
+} else {
+console.log('n is an odd number');
+}
+//<< 'n is an even number'
+
+
+/*
+Ternary Operator
+1. A shorthand way of writing an if ... else statement is to use the ternary operator, ? , which takes 
+three operands in the following format:
+2.
+*/
+
+/*
+condition ? (//code to run if condition is true) : (//code to run if condition is false)
+*/
+
+/*
+const n = 5;
+n%2 === 0 ? console.log('n is an even number') : console.log('n is an odd number');
+//<< 'n is an odd number'
+*/
+
+
+/*
+ Switch Statements
+ 1. 
+*/
+const number = 4;
+if (number === 4) {
+   console.log('You rolled a four');
+   } else if (number === 5) {
+   console.log('You rolled a five');
+   } else if(number === 6){
+   console.log('You rolled a six');
+   } else {
+   console.log('You rolled a number less than four');
+   }
+
+
+   switch (number) {
+      case 4:
+      console.log('You rolled a four');
+      break;
+      case 5:
+      console.log('You rolled a five');
+      break;
+      case 6:
+      console.log('You rolled a six');
+      break;
+      default:
+      console.log('You rolled a number less than four');
+      break;
+  }
+
+
+  /*
+  while loops
+  */
+  let bottles = 10;
+  while (bottles > 0){
+      console.log(`There were ${bottles} green bottles, hanging on a wall. And if one green bottle should 
+      accidentally fall, there'd be ${bottles-1} green bottles hanging on the wall`);
+      bottles--;
+  }
+
+  const otherLoop = 'Other Loops';
+  console.log(otherLoop);
+
+  let bottles_1 = 11;
+while (--bottles_1){
+    console.log(`There were ${bottles_1} green bottles, hanging on a wall. And if one green bottle should 
+    accidentally fall, there'd be ${bottles_1-1} green bottles hanging on the wall`);
+}
+
+
+/*
+infinite loops
+1. It is important that the condition in a while loop will be met at some point, otherwise your code will get 
+stuck in an infinite loop that could possibly crash the program.
+2. 
+*/
+
+/*
+let m = 1;
+while(m > 0){
+    console.log('Hello');
+    m++;
+}
+*/
+
+
+const otherLoop_1 = 'Other Loops';
+console.log(otherLoop_1);
+
+
+/*
+do ... while Loops
+1. A do ... while loop is similar to a while loop. The only difference is that the condition comes after the 
+block of code:
+2. This means that the block of code will always be run at least once, regardless of the condition being true 
+or not.
+ */
+let bottles_2 = 10;
+do {
+    console.log(`There were ${bottles_2} green bottles, hanging on a wall. And if one green bottle should 
+    accidentally fall, there'd be ${bottles_2-1} green bottles hanging on the wall`);
+    bottles_2--;
+} while (bottles_2 > 0)
+
+
+const otherLoop_2 = 'Other Loops';
+console.log(otherLoop_2);
+
+
+/*
+For Loops
+1. For loops are probably the most commonly type of loop used in JavaScript
+2. The initialization code is run before the loop starts and is usually employed to initialize any variables 
+used in the loop. The condition has to be satisfied for the loop to continue. 
+3. The after code is what to do afte reach iteration of the loop, and it is typically used to increment a 
+counter of some sort.
+*/
+for (let bottles_3 = 10 ; bottles_3 > 0 ; bottles_3--) {
+   console.log(`There were ${bottles_3} green bottles, hanging on a wall. And if one green bottle should 
+   accidentally fall, there'd be ${bottles_3-1} green bottles hanging on the wall`);
+} 
+
+
+const otherLoop_3 = 'Other Loops';
+console.log(otherLoop_3);
+
+
+let bottles_4 = 10; // bottles is initialized here instead
+for ( ; bottles_4 > 0 ; ) { // empty initialization and increment
+console.log(`There were ${bottles_4} green bottles, hanging on a wall. And if one green bottle should 
+accidentally fall, there'd be ${bottles_4-1} green bottles hanging on the wall`);
+bottles_4--; // increment moved into code block
+}
+
+
+/*
+nested for loops
+
+*/
+for(let i=1 ; i<13 ; i++){
+for(let j=1 ; j<13 ; j++){
+    console.log(`${j} multiplied by ${i} is ${i*j}`);
+    }
+}
+
+
+
+/*
+Looping over Arrays
+1. A for loop can be used to iterate over each value in an array. If we take our avengers array example from 
+earlier, we can create a for loop that outputs each item in the array to the console using the following loop:
+2. MAX is a variable.
+3. 
+*/
+for(let i=0, max=avengers.length; i < max; i++){
+    console.log(avengers[i]);
+    }
+
+/*
+1. ES6 introduced an improved iterator function for arrays called a for-of loop that uses a slightly different 
+syntax:
+2. 
+*/
+for(const value of avengers){
+    console.log(value);
+    }
+
+
+/*
+Looping Over Sets
+1. Sets are enumerable, which means they have methods that allow you to loop over each value in the set. 
+2. The loop will iterate over each value in the same order they were added to the set. 
+*/
+const letters = new Set('hello');
+for(const letter of letters) {
+    console.log(letter);
+}
+
+
+/*
+Looping Over Maps
+1. Maps are also enumerable, so it's also possible to loop over a map in a similar way to a set. 
+2. The loop will iterate over each key-value pair in the same order as they were added to the map.
+*/
+const romanNumerals_1 = new Map();
+romanNumerals_1.set(1,'I').set(2,'II').set(3,'III').set(4,'IV').set(5,'V');
+for(const key of romanNumerals_1.keys()) {
+    console.log(key);
+}
+
+const romanNumerals_2 = new Map();
+romanNumerals_2.set(1,'I').set(2,'II').set(3,'III').set(4,'IV').set(5,'V');
+for(const value of RomanNumerals_2.values()) {
+    console.log(value);
+}
+
+/*
+for(const [key,value] of RomanNumerals.entries()) {
+    console.log(`${key} in Roman numerals is ${value}`);
+}
+*/
 
 
 
