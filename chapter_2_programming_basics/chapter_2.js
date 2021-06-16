688fe79e3c2dcbb01d13e5966dd18f0ac21ffe6d
djhi12 <ido20002@byui.edu>
2021-06-16 22:04:21 +0800

    initial commit

--- chapter_2_programming_basics/chapter_2.js
+++
@@ -1,7 +0,0 @@
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert('You answered' + ' ' + answer);
-console.log(answer);
-
-//const question = 'What is Superman's real name?';
-
