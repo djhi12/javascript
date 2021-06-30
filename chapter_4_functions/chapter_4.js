a9798c042fd594c44c267867e78d839ef06a7b31
djhi12 <ido20002@byui.edu>
2021-06-30 22:00:22 +0800

    initial commit

--- chapter_4_functions/chapter_4.js
+++ chapter_4_functions/chapter_4.js
@@ -310,3 +310,258 @@ console.log(name); // will return undefined before assignment
 // variable is defined here
 var name = 'Alexa';
 console.log(name); // will return 'Alexa' after assignment
+
+
+// the variable helloExpression has a value of undefined, so the function cannot be invoked
+//helloExpression(); // throws an error
+// the function declaration can be invoked before it is declared
+helloDeclaration(); // returns 'hello'
+// assign function expression to a variable
+var helloExpression = function() { 
+    console.log('hello') 
+}
+// declare function declaration
+function helloDeclaration() {
+    console.log('hello') 
+}
+// The function expression can only be invoked after assignment
+helloExpression(); // returns 'hello'
+
+
+/*
+Callbacks
+1. This means that functions can also be given as a parameter to another function.
+2. A function that is passed as an argument to another is known as a callback.
+*/
+
+
+function sing(song) {
+    console.log(`I'm singing along to ${song}`);
+}
+sing('Let It Go') //'I'm singing along to Let It Go'
+sing('Let It Go',dance);
+
+// The callback is provided as a parameter, then invoked inside the body of the function.
+function sing_1(song,callback) {
+    console.log(`I'm singing along to ${song}.`);
+    return callback(song, callback);
+}
+
+function dance() {
+    console.log("I'm moving my body to the groove."); 
+    //( We’re just logging a simple message to the console in these examples, but these functions could be used to do anything in a practical sense.)
+    }
+
+sing_1('Let It Go, Let it go',dance);
+
+/*
+1. It is possible to check if an argument is a function using the following code:
+*/
+/*
+if(typeof(callback) === 'function'){
+    callback();
+}
+*/
+
+
+function dance() {
+    console.log("I'm moving my body to the groove."); 
+    //( We’re just logging a simple message to the console in these examples, but these functions could be used to do anything in a practical sense.)
+    }
+
+
+sing('Let It Go',()=>{ console.log("I'm standing on my head.");});
+
+
+/*
+Sorting Arrays With A Callback
+1. In the last chapter we saw that arrays have a sort() method that sorted the items in the 
+array into alphabetical order. This is fine for strings, but you might recall that it didn't 
+work so well for numbers:
+*/
+
+//This function can now be used as a callback in the sort() method to sort the array of numbers correctly:
+const numbersArray =  [1, 3, 12, 5, 23, 18, 7].sort(numerically);
+console.log(numbersArray);
+
+/*
+1. This simply subtracts the two numbers that are being compared, giving a result that is either 
+negative (if b is bigger than a ), zero (if a and b are the same value), or positive (if a is bigger than b ).
+*/
+function numerically(a, b) {
+    return a - b;
+}
+
+/*
+overflow errors
+1. In some rare instances where an array includes some very large and negative numbers, an 
+overflow error can occur and the result of a-b becomes smaller than the smallest number 
+that JavaScript is able to cope with. If this is the case, the following function can be 
+used as a callback instead:
+*/
+function numerically (a, b){
+    if (a < b) {
+        return -1;
+    } else if(a > b) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+console.log(numerically(1,2));
+
+
+/*
+Array Iterators
+1. Arrays have a number of methods that utilize callbacks to make them more flexible.
+*/
+
+
+/*
+forEach()
+1. 
+*/
+const colors = ['Red', 'Green', 'Blue']
+for(let i = 0, max = colors.length ; i < max ; i++ ) {
+    console.log(`Color at position ${i} is ${colors[i]}`);
+}
+//'Color at position 0 is Red'
+//'Color at position 1 is Green'
+// 'Color at position 2 is Blue'
+
+{
+    const otherLoop = 'Other Loop';
+    console.log(otherLoop);
+}
+
+/*
+1. An alternative is to use the forEach() method. This will loop through the array and invoke a 
+callback function using each value as an argument.
+2. The callback function takes three parameters, the first represents the value in the array, 
+the second represents the current index and the third represent the array that the callback is being called on. 
+*/
+colors.forEach( (color,index) =>
+    console.log(`Color at position ${index}  is ${color}`) );
+
+
+
+/*
+map()
+1. The map() method is very similar to the forEach() method. It also iterates over an array, and 
+takes a callback function as a parameter that is invoked on each item in the array. This is often 
+used to process data returned from databases in array form, such as adding HTML tags to plain text.
+2. The difference is that it returns a new array that replaces each value with the return value of
+the callback function.
+*/
+
+/* 
+1. For example, we can square every number in an array using the square function we wrote previously as 
+a callback to the map() method:
+*/
+const numberArray = [1,2,3].map( square );
+console.log(numbersArray); // 1, 3, 5, 7, 12, 18, 23
+
+// An anonymous function can also be used as a callback. This example will double all the numbers in the array:
+const numberArray_1 = [1,2,3].map(x => 2 * x);
+console.log(numberArray_1); // 2, 4, 6
+
+// The next example takes each item in the array and places them in uppercase inside paragraph HTML tags:
+const colorArray = ['red','green','blue'].map( color => `<p> ${color.toUpperCase()}</p>` );
+console.log(colorArray);
+document.getElementById("colorArray").innerHTML = colorArray;
+
+
+/*
+1. Notice in this and the previous example, the anonymous function takes a parameter, color , 
+which refers to the item in the array. This callback can also take two more parameters ― 
+the second parameter refers to the index number in the array and the third refers to the array itself.
+2. It's quite common for callbacks to only used the first, index, parameter, but the next 
+example shows all three parameters being used:
+*/
+const colorArray_1 = ['red','green','blue'].map( (color, index, array) => `Element ${index} is ${color}. 
+There are ${array.length} items in total.` );
+console.log(colorArray_1);
+
+
+/*
+Reduce()
+1. The reduce() method is another method that iterates over each value in the array, but this time it 
+cumulatively combines each result to return just a single value.
+2.  The callback function is used to describe how to combine each value of the array with the 
+running total. This is often used to calculate statistics such as averages from data stored in 
+an array. It usually takes two parameters. The first parameter represents the accumulated value 
+of all the calculations so far, and the second parameter represents the current value in the array. 
+The following example shows how to sum an array of numbers:
+*/
+const number_4 = [1,2,3,4,5].reduce( (acc,val) => acc + val );
+console.log(number_4); // 15
+
+/*
+1. The reduce() method also takes a second parameter after the callback, which is the initial value 
+of the accumulator, acc . For example, we could total the numbers in an array, but starting at 10, instead of zero:
+*/
+const number_5 = [1,2,3,4,5].reduce( (acc,val) => acc + val,10); // <---- second parameter of 10 here
+console.log(number_5); // 25
+
+// Another example could be to calculate the average word length in a sentence:
+const sentence = 'The quick brown fox jumped over the lazy dog';
+console.log(sentence); // The quick brown fox jumped over the lazy dog
+
+//
+const sentence_1 = 'The quick brown fox jumped over the lazy dog';
+const sentence_2 = sentence_1.split();
+console.log(sentence_2); // ["The quick brown fox jumped over the lazy dog"]
+
+const sentence_3 = sentence_1.split(" ");
+console.log(sentence_3); // ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+/* 
+const totalWords = sentence.reduce((acc,word) => acc + word.length, 0);
+console.log(totalWords); // sentence.reduce is not a function
+*/
+
+
+/*
+Filter()
+1. The filter() method returns a new array that only contains items from the original array that 
+return true when passed to the callback.
+*/
+// For example, we can filter an array of numbers to just the even numbers using the following code:
+const numbers = [ 2, 7, 6, 5, 11, 23, 12 ];
+console.log(numbers.filter(x => x % 2 === 0 )); // this returns true if the number is even // 2, 6, 12
+//console.log(numbers); // 2, 7, 6, 5,11, 23, 12
+
+
+// The filter() method provides a useful way of finding all the truthy values from an array:
+const array = [ 0, 1, '0', false, true, 'hello' ];
+console.log(array.filter(Boolean)); // [1, "0", true, "hello"]
+
+// To find all the falsy values, the following filter can be used:
+console.log(array.filter(x => !x)); // [0, false]
+
+
+/*
+Chaining Iterators Together
+1. The various iterator functions can be used in combination to create some powerful transformations 
+of data stored in arrays. This is achieved by a process called chaining methods together.
+2. Chaining works because the iterator functions return an array, which means that another iterator 
+function can then be chained on to the end and it will be applied to the new array.
+*/
+
+/* For example, we can calculate the sum of square numbers using the map() method to square 
+each number in the array and then chain the reduce() method on the end to add the results together:
+*/
+const numbers_1 = [1,2,3].map( x => x*x ).reduce((acc,x) => acc + x );
+console.log(numbers_1); // 14
+
+
+/*
+1. Another more complex example could be used to take an array of orders, apply a sales tax to 
+them using map() and then use reduce() to find the total:
+*/
+const sales = [ 100, 230, 55];
+const sales_1 = totalAfterTaxSales = sales.map( (amount) => amount * 1.15 ).reduce( (acc,val) => acc + val );
+console.log(sales_1); // 442.75
+
+
+
