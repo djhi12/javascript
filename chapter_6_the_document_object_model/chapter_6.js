5cb037a1f875e838974644601cea3c0282d50085
djhi12 <ido20002@byui.edu>
2021-08-02 21:53:51 +0800

    initial commit

--- chapter_6_the_document_object_model/chapter_6.js
+++ chapter_6_the_document_object_model/chapter_6.js
@@ -1,10 +1,34 @@
+/*
+What is the DOM?
+*/
+
+/*
+History of the DOM
+*/
+
+/*
+An Example Web Page
+*/
+
+/*
+Getting Elements
+*/
 const body = document.body;
+console.log(body); // 
+console.log(typeof body); // object
+document.getElementById("body").innerHTML = body;
 
-const h1 = document.getElementById('title');
+// HTML DOM nodeType Property
+console.log(body.nodeType); // 1
+// console.log(header.nodeType); // Uncaught ReferenceError: header is not defined
 
+
+
+
+// const h1 = document.getElementById('title');
 // Document.form
-var selectForm = document.forms[index];
-var selectFormElement = document.forms[index].elements[index];
-var formResult = selectForm + selectFormElement;
-console.log(formResult);
+// var selectForm = document.forms[index]; // Uncaught ReferenceError: index is not defined
+// var selectFormElement = document.forms[index].elements[index]; // Uncaught ReferenceError: index is not defined
+// var formResult = selectForm + selectFormElement; // Uncaught ReferenceError: selectForm is not defined
+// console.log(formResult); // Uncaught ReferenceError: formResult is not defined
 
