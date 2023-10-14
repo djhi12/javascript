9140ae81e03482f6692ecc8a8644d614dcc7796d
djhi12 <ido20002@byui.edu>
2023-10-14 16:50:40 +0800

    Initial commit

---
+++ dataTypes.js
@@ -0,0 +1,41 @@
+
+
+let num = 42;
+let pi = 3.14159;
+
+
+let message = "Hello, world!";
+let name = 'Alice';
+
+
+
+let isTrue = true;
+let isFalse = false;
+
+
+
+let uninitialized;
+console.log(uninitialized); // undefined
+
+
+
+let empty = null;
+
+
+
+const sym = Symbol("description");
+
+
+const bigNum = 1234567890123456789012345678901234567890n;
+
+
+const person = {
+    name: "John",
+    age: 30,
+    isStudent: false,
+};
+
+
+const colors = ["red", "green", "blue"];
+
+
