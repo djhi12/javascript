2beb5b5d39df166e9bb9cdd4df61c42751d7a5ba
djhi12 <ido20002@byui.edu>
2023-10-16 21:30:46 +0800

    Initial commit

--- dataTypes.js
+++ dataTypes.js
@@ -1,41 +1,37 @@
 
-
+// Numbers
 let num = 42;
 let pi = 3.14159;
 
-
+// String
 let message = "Hello, world!";
 let name = 'Alice';
 
-
-
+// Boolean
 let isTrue = true;
 let isFalse = false;
 
-
-
+// Undefined
 let uninitialized;
 console.log(uninitialized); // undefined
 
-
-
+// Null
 let empty = null;
 
-
-
+// Symbol
 const sym = Symbol("description");
 
-
+// BigInt
 const bigNum = 1234567890123456789012345678901234567890n;
 
-
+// Object 
 const person = {
     name: "John",
     age: 30,
     isStudent: false,
 };
 
-
+// Array
 const colors = ["red", "green", "blue"];
 
 
