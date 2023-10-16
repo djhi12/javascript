582aed2985fad8a2246a45da6ed2d32914f73e9a
djhi12 <ido20002@byui.edu>
2023-10-16 21:56:13 +0800

    Initial commit

---
+++ numbers.js
@@ -0,0 +1,39 @@
+
+// Number Data Type
+var integerNumber = 42; // Integer
+var floatingPointNumber = 3.14; // Floating-point number
+
+// Numeric Operations
+var result = 10 + 5; // Addition
+var difference = 20 - 8; // Subtraction
+var product = 6 * 7; // Multiplication
+var quotient = 12 / 4; // Division
+
+// Arithmetic Operators
+
+// Math Object
+var squareRoot = Math.sqrt(16); // Calculates the square root
+var randomValue = Math.random(); // Generates a random number between 0 and 1
+
+// NaN (Not-a-Number)
+var result = 0 / 0; // NaN
+
+// Infinity and -Infinity
+var positiveInfinity = Infinity;
+var negativeInfinity = -Infinity;
+
+// Type Coercion
+var result = "42" + 2; // "422" (string concatenation)
+
+// Number Conversions
+var stringNumber = "42";
+var parsedNumber = parseInt(stringNumber); // 42
+var floatNumber = parseFloat("3.14"); // 3.14
+
+// Number Precision
+
+// Comparison of Numbers
+var num1 = 0.1 + 0.2;
+var num2 = 0.3;
+var tolerance = 0.0001;
+var isEqual = Math.abs(num1 - num2) < tolerance; // Check if the numbers are approximately equal
