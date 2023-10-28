5d1f3024ffc9314cb5d74cfaa51e25ad80c1197b
djhi12 <ido20002@byui.edu>
2023-10-28 16:36:38 +0800

    Initial commit

---
+++ strings.js
@@ -0,0 +1,28 @@
+
+// Creating Strings:
+let singleQuotedString = 'This is a single-quoted string.';
+let doubleQuotedString = "This is a double-quoted string.";
+
+// String Concatenation:
+let firstName = 'John';
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName;
+
+// String Length:
+let text = 'Hello, World!';
+let length = text.length; // Returns 13
+
+// Accessing Characters:
+let text2 = 'Hello';
+let firstCharacter = text2[0]; // Returns 'H'
+let thirdCharacter = text2[2]; // Returns 'l'
+
+// String Methods:
+let text3 = 'Hello, World!';
+let uppercaseText = text3.toUpperCase(); // Converts to uppercase
+let lowercaseText = text3.toLowerCase(); // Converts to lowercase
+let slicedText = text3.slice(0, 5); // Slices the string
+let indexOfComma = text3.indexOf(','); // Finds the index of a substring
+let replacedText = text3.replace('World', 'Universe'); // Replaces a substring
+
+
