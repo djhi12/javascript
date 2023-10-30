06ec2b1a413b9f6a4c327866c0c8ae20d821fcb5
djhi12 <ido20002@byui.edu>
2023-10-30 21:16:13 +0800

    Comments

---
+++ comments.js
@@ -0,0 +1,32 @@
+
+// Single-line comments:
+// This is a single-line comment
+var x = 10; // You can also add comments at the end of a line of code
+
+
+// Multi-line comments:
+/*
+This is a
+multi-line comment
+*/
+var y = 20;
+
+/*
+* You can also format
+* multi-line comments
+* like this
+*/
+var z = 30;
+
+
+// Example
+// Calculate the sum of two numbers
+function addNumbers(a, b) {
+    return a + b; // Return the result
+}
+
+/* 
+This function takes two parameters, a and b, and returns their sum.
+It can be used like this:
+var result = addNumbers(5, 7);
+*/
