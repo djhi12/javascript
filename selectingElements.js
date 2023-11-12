40d57bd0f156c600bc9828f347f9fb1245adc316
djhi12 <ido20002@byui.edu>
2023-11-12 20:24:13 +0800

    Selecting Elements

---
+++ selectingElements.js
@@ -0,0 +1,35 @@
+
+// getElementById():
+var element = document.getElementById("elementId");
+
+
+// getElementsByClassName():
+var elements = document.getElementsByClassName("className");
+
+
+// getElementsByTagName():
+var elements = document.getElementsByTagName("tagName");
+
+
+// querySelector():
+var element = document.querySelector("cssSelector");
+
+
+// querySelectorAll():
+var elements = document.querySelectorAll("cssSelector");
+
+
+// getElementsByName():
+var elements = document.getElementsByName("nameAttribute");
+
+
+// parentNode, children, firstElementChild, lastElementChild:
+var parent = element.parentNode;
+var children = element.children;
+var firstChild = parent.firstElementChild;
+var lastChild = parent.lastElementChild;
+
+
+// nextElementSibling, previousElementSibling:
+var nextElement = element.nextElementSibling;
+var prevElement = element.previousElementSibling;
