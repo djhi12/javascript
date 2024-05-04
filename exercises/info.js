389f1366ed4849b3105e144ad6aaa7493c3fdf86
djhi12 <ido20002@byui.edu>
2024-05-04 16:38:22 +0800

    udated

---
+++ exercises/info.js
@@ -0,0 +1,13 @@
+// Pre-defined information
+let name = "John Doe";
+let age = 30;
+let address = "123 Main St, Anytown";
+let favoriteFood = "Pizza";
+let favoriteBook = "The Great Gatsby";
+
+// Print information
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Favorite Food:", favoriteFood);
+console.log("Favorite Books:", favoriteBooks);
